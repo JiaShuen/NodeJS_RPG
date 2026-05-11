a1a4a75c82b870d119930bdfd13fca1f4cd9eda3
Js <jiashuenpoly@gmail.com>
2026-05-12 07:55:33 +0800

    Completed looping function of game

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const readline = require('readline-sync');
 
 var hero = new character("Hero", 3, 0);
 var demonKing = new character("Demon King", 3, 0);
+var continueGame = true;
 
 function getRandomInt() {
   return Math.floor(Math.random() * 10);
@@ -17,18 +18,32 @@ if (userIn == '2') {
     console.log("Exiting the game...");
     setTimeout(() => {
         process.exit(0);
-    }, 1000);
+    }, 500); // 500 milliseconds delay before exiting
 };
 
 var atkhero = getRandomInt();
 var atkdemon = getRandomInt();
 
-if (atkhero > atkdemon) {
-    demonKing.hp -= 1;
-    console.log("You attacked the Demon King! The Demon King has " + demonKing.getHp() + " HP left.");
-} else if (atkhero < atkdemon) {
-    hero.hp -= 1;
-    console.log("The Demon King attacked you! You have " + hero.getHp() + " HP left.");
-} else {
-    console.log("You and the Demon King attacked each other at the same time! No damage was dealt.");
-}
+do {
+    var atkhero = getRandomInt();
+    var atkdemon = getRandomInt();
+
+    console.log("Hero ATK: " + atkhero + " | Demon King ATK: " + atkdemon);
+
+    setTimeout(() => {
+        if (atkhero > atkdemon) {
+            demonKing.hp -= 1;
+            console.log("You attacked the Demon King! The Demon King has " + demonKing.getHp() + " HP left.");
+        } else if (atkhero < atkdemon) {
+            hero.hp -= 1;
+            console.log("The Demon King attacked you! You have " + hero.getHp() + " HP left.");
+        } else {
+            console.log("You and the Demon King attacked each other at the same time! No damage was dealt.");
+        }
+
+        if (hero.getHp() == 0 || demonKing.getHp() == 0) {
+            continueGame = false;
+        }
+    }, 500)
+
+} while (continueGame);
