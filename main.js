8e9dd941fa9c3efb5b3df3dd442b0d6c0a7f6cb8
Js <jiashuenpoly@gmail.com>
2026-05-12 13:27:26 +0800

    Added input validation

--- main.js
+++ main.js
@@ -9,17 +9,35 @@ function getRandomInt() {
   return Math.floor(Math.random() * 10);
 }
 
+function checkInput(input, posVal) {
+    var pass = true;
+    if (!Number.isInteger(parseInt(input))) {
+        pass = false;
+        return pass;
+    } else {
+        for(let i = 0; i < posVal.length; i++) {
+            if (input == posVal[i]) {
+                return pass;
+            }
+        }
+        pass = false;
+        return pass;
+    }
+}
+
 
 // Start of program
-console.log("<<<Welcome to Fantasy RPG!>>>\nPress (1) to start the game.\nPress (2) to exit.");
-var userIn = readline.question(">>> ");
+do {
+    console.log("<<<Welcome to Fantasy RPG!>>>\nPress (1) to start the game.\nPress (2) to exit.");
+    var userIn = readline.question(">>> ");
 
-if (userIn == '2') {
-    console.log("Exiting the game...");
-    setTimeout(() => {
+    var checkUserIn = checkInput(userIn, [1, 2]);
+
+    if (checkUserIn && userIn == '2') {
+        console.log("Exiting the game...");
         process.exit(0);
-    }, 500); // 500 milliseconds delay before exiting
-};
+    };
+} while(!checkUserIn);
 
 var atkhero = getRandomInt();
 var atkdemon = getRandomInt();
@@ -30,20 +48,18 @@ do {
 
     console.log("Hero ATK: " + atkhero + " | Demon King ATK: " + atkdemon);
 
-    setTimeout(() => {
-        if (atkhero > atkdemon) {
-            demonKing.hp -= 1;
-            console.log("You attacked the Demon King! The Demon King has " + demonKing.getHp() + " HP left.");
-        } else if (atkhero < atkdemon) {
-            hero.hp -= 1;
-            console.log("The Demon King attacked you! You have " + hero.getHp() + " HP left.");
-        } else {
-            console.log("You and the Demon King attacked each other at the same time! No damage was dealt.");
-        }
+    if (atkhero > atkdemon) {
+        demonKing.hp -= 1;
+        console.log("You attacked the Demon King! The Demon King has " + demonKing.getHp() + " HP left.");
+    } else if (atkhero < atkdemon) {
+        hero.hp -= 1;
+        console.log("The Demon King attacked you! You have " + hero.getHp() + " HP left.");
+    } else {
+        console.log("You and the Demon King attacked each other at the same time! No damage was dealt.");
+    }
 
-        if (hero.getHp() == 0 || demonKing.getHp() == 0) {
-            continueGame = false;
-        }
-    }, 500)
+    if (hero.getHp() == 0 || demonKing.getHp() == 0) {
+        continueGame = false;
+    }
 
 } while (continueGame);
